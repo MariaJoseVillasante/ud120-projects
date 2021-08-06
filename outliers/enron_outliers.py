bbefda5711a80a8e022cba38e389cbba4ce4c1d0
Maria Jose Villasante <85708732+MariaJoseVillasante@users.noreply.github.com>
2021-08-06 10:31:51 -0400

    Update enron_outliers.py

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -15,5 +15,19 @@ data = featureFormat(data_dict, features)
 
 ### your code below
 
+maxsal = max(data[:,0])
+maxbon = max(data[:,1])
+print(maxsal)
+print(maxbon)
+
+
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
 
